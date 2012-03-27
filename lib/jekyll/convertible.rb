9341ed41e1b8dcc349ad045eb14a4dd87e2c2121
Baris Balic <baris.balic@uswitch.com>
2012-03-27 17:03:52 +0100

    Added some additional output so that we can see what file is breaking the parser.

--- lib/jekyll/convertible.rb
+++ lib/jekyll/convertible.rb
@@ -25,6 +25,7 @@ module Jekyll
     #
     # Returns nothing.
     def read_yaml(base, name)
+      STDOUT.puts "[#{Time.now}] Parsing #{File.join(base, name)}"
       self.content = File.read(File.join(base, name))
 
       begin
